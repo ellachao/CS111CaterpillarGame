a68f76e40a4df69b8f029e9053579d4bc3fd9044
guiguiguiguigui <sophiaphia@live.com>
2015-04-29 18:08:47 -0400

    change dir
    sub code

--- changeDir.py
+++ changeDir.py
@@ -1,35 +1,6 @@
-import Tkinter as tk
-import time
+
 import random
-
-class App(tk.Frame):
-    def __init__(self,myRoot):
-        tk.Frame.__init__(self)
-        self.root=myRoot
-        myRoot.title('Caterpillar Game')
-        myRoot.geometry('600x500')
-        self.start=True
-        self.gameOver=False
-        self.grid()
-        self.createWidgetStart()
-    
-    def createWidgetStart(self):
-        pic=tk.PhotoImage(file='caterpillar.gif')
-
-        picLabel=tk.Label(self,image=pic)
-        picLabel.pic=pic
-        picLabel.grid(row=1, column=1)
-        self.startButton=tk.Button(self,text='Start')
-        self.startButton.grid(row=10,column=1)
-
-
-root= tk.Tk()
-app=App(root)
-root.mainloop()
-
-#
-#import random
-#import Tkinter  as tk
+import Tkinter  as tk
 
 boardRows = 10
 boardCols = 10
